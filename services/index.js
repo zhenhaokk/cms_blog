f9b5867f47e6c23ab24f30c6b46cc0d7d8bcbe86
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-22 22:55:09 +1000

    five

--- services/index.js
+++ services/index.js
@@ -82,3 +82,18 @@ export const getSimilarPosts = async (category, slug) => {
     
   return result.posts;
 }
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+};
