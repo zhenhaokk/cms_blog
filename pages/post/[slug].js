2b6cb3f1c651d121bc561edc8ad308c7738055cf
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-23 02:04:02 +1000

    ten

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -14,6 +14,11 @@ import { getPosts, getPostDetails } from "../../services";
 // import { AdjacentPosts } from "../../sections";
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
   return (
     <>
       <div className="container mx-auto px-10 mb-8">
