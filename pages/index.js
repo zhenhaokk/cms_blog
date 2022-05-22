19f7bbe46cf9a632d2427b8516b5cfa86f79a9bf
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-23 02:00:57 +1000

    ning

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components";
 import { getPosts } from "../services";
 
+import { FeaturedPosts } from "../sections";
+
 export default function Home({ posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -9,6 +11,7 @@ export default function Home({ posts}) {
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
